c98b4f1d83a399d8536465a1446760ec850a97ba
sakhawat777 <biplobbms@gmail.com>
2023-02-16 11:55:30 +0600

    Sign in a user with an email address and password Done

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './auth.module.scss'
 import loginImg from '../../assets/login.png'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaGoogle } from 'react-icons/fa';
 import Card from './../../components/card/Card';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/config';
+import { toast } from 'react-toastify';
+import Loader from './../../components/loader/Loader';
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+  const loginUser = (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      setIsLoading(false)
+      toast.success("Login successful...")
+      navigate("/")
+    })
+    .catch((error) => {
+      setIsLoading(false)
+      toast.error(error.message)
+    });
+  }
   return (
+    <>
+     {isLoading && <Loader/>}
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
         <img src={loginImg} alt="Login" width="400" />
@@ -15,10 +41,10 @@ const Login = () => {
       <div className={styles.form}>
             <h2>Login</h2>
            
-            <form>
-                <input type="text" placeholder='Email' required/>
-                <input type="password" placeholder='Password' required/>
-                <button className="--btn --btn-primary --btn-block">Login</button>
+            <form onSubmit={loginUser}>
+                <input type="text" placeholder='Email' required value={email} onChange={(e) =>setEmail(e.target.value) }/>
+                <input type="password" placeholder='Password' required value={password} onChange={(e) =>setPassword(e.target.value)}/>
+                <button type='submit' className="--btn --btn-primary --btn-block">Login</button>
                 <div className={styles.links}>
                     <Link to="/reset">Reset Password</Link>
                 </div>
@@ -32,6 +58,7 @@ const Login = () => {
       </div>
       </Card>
     </section>
+    </>
   )
 }
 
