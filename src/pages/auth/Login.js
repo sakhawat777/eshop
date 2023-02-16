61c3ac00d7ef8b12370263b963f951ad2b85e06b
sakhawat777 <biplobbms@gmail.com>
2023-02-16 13:13:39 +0600

    Login with Google Done

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -4,7 +4,7 @@ import loginImg from '../../assets/login.png'
 import { Link, useNavigate } from 'react-router-dom';
 import { FaGoogle } from 'react-icons/fa';
 import Card from './../../components/card/Card';
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth } from '../../firebase/config';
 import { toast } from 'react-toastify';
 import Loader from './../../components/loader/Loader';
@@ -30,6 +30,22 @@ const Login = () => {
       toast.error(error.message)
     });
   }
+  // Login with Google
+  const provider = new GoogleAuthProvider();
+
+  const singInWithGoogle = (e) => { 
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      toast.success("Login successful")
+      navigate("/")
+     
+    }).catch((error) => {
+      // Handle Errors here.
+      toast.error(error.message)
+    });
+  
+  }
   return (
     <>
      {isLoading && <Loader/>}
@@ -50,7 +66,7 @@ const Login = () => {
                 </div>
                 <p>-- or --</p>
             </form>
-            <button className="--btn --btn-danger --btn-block"><FaGoogle color='#fff'/> Login With Google</button>
+            <button className="--btn --btn-danger --btn-block" onClick={singInWithGoogle}><FaGoogle color='#fff'/> Login With Google</button>
             <span className={styles.register}>
                 <p>Don't have an account?</p>
                 <Link to="/register">Register</Link>
